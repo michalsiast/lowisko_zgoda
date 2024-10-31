fefebbe7b20f8e1dd71e18aeae287aeb6fa6090e
Michał Siast <michalsiast@gmail.com>
2024-10-31 20:19:02 +0100

    t

--- src/resources/views/vendor/notifications/email.blade.php
+++ src/resources/views/vendor/notifications/email.blade.php
@@ -1,17 +1,1 @@
-@component('mail::message')
-    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 150px; height: auto;">
-
-    # Witaj!
-
-    Otrzymałeś tę wiadomość, ponieważ otrzymaliśmy prośbę o zresetowanie hasła dla Twojego konta.
-    @component('mail::button', ['url' => $actionUrl])
-        Zresetuj Hasło
-    @endcomponent
-
-    Ten link do resetowania hasła wygasa za 60 minut.
-
-    Jeśli nie żądałeś zresetowania hasła, nie musisz podejmować żadnych działań.
-
-    Pozdrawiamy,<br>
-    Łowisko ZGODA
-@endcomponent
+test
