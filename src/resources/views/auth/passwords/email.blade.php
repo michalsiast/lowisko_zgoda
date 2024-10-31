12d7d90d627d794e026137db786895e51fb954a8
Michał Siast <michalsiast@gmail.com>
2024-10-31 18:26:31 +0100

    test

--- src/resources/views/auth/passwords/email.blade.php
+++ src/resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,25 @@
 @extends('default.layout')
 
 @section('content')
+    <div class="inner-hero">
+        <div class="container">
+            <div class="rwo">
+                <div class="col-lg-8 m-auto text-cetner">
+                    <div class="main-heading">
+                        <h1>Resetowanie hasła</h1>
+                        <div class="page-intro">
+                            <a href="/">Strona główna</a>
+                            <span><i class="fa-solid fa-angle-right"></i></span>
+                            <p>Resetowanie hasła</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="{{ asset('images/shapes/inner-hero-shape1.png') }}" alt="" class="shape1">
+        <img src="{{ asset('images/shapes/inner-hero-shape2.png') }}" alt="" class="shape2">
+        <img src="{{ asset('images/shapes/inner-hero-shape3.png') }}" alt="" class="shape3">
+    </div>
     <div class="container">
         <h2>Resetowanie hasła</h2>
         <form method="POST" action="{{ route('password.email') }}">
