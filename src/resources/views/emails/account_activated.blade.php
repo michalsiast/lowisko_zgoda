9b61d6cc52351f085af4017879772375cfe34b83
Michał Siast <michalsiast@gmail.com>
2025-05-07 20:50:44 +0200

    Update account_activated.blade.php

--- src/resources/views/emails/account_activated.blade.php
+++ src/resources/views/emails/account_activated.blade.php
@@ -1,16 +1,12 @@
-@component('mail::message')
-    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 100px; height: auto;">
+Witaj, {{ $user->name }}!
 
-    # Witaj, {{ $user->name }}!
+Twoje konto zostało pomyślnie aktywowane! Możesz teraz zalogować się do systemu.
 
-    Twoje konto zostało pomyślnie aktywowane! Możesz teraz zalogować się do systemu.
-
-    @component('mail::button', ['url' => route('user.login')])
-        Zaloguj się
-    @endcomponent
-
-    Cieszymy się, że dołączyłeś do naszej społeczności!
-
-    Pozdrawiamy,<br>
-    Łowisko ZGODA
+@component('mail::button', ['url' => route('user.login')])
+    Zaloguj się
 @endcomponent
+
+Cieszymy się, że dołączyłeś do naszej społeczności!
+
+Pozdrawiamy,<br>
+Łowisko ZGODA
